030d1d838109f48528a08a710fbb22ef52a4e97b
Nika Perfilova <ees2009.2014@gmail.com>
2022-01-24 10:36:40 +0700

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,46 +1,77 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
+};
+
+const binToChar = {
+  10: '.',
+  11: '-',
 };
 
 function decode(expr) {
-    // write your solution here
+  const chunks = [];
+  for (let i = 0; i < expr.length; i += 10) {
+    const chunk = [];
+    for (let j = i; j < i + 10; j += 2) {
+      const smallChunk = expr.slice(j, j + 2);
+      chunk.push(smallChunk);
+    }
+    chunks.push(chunk);
+  }
+
+  const letters = [];
+
+  for (const chunk of chunks) {
+    if (chunk.every((code) => code === '**')) {
+      letters.push(' ');
+      continue;
+    }
+    let encodedLetter = '';
+    for (const code of chunk) {
+      if (binToChar[code]) {
+        encodedLetter += binToChar[code];
+      }
+    }
+    letters.push(MORSE_TABLE[encodedLetter]);
+  }
+
+  return letters.join('');
 }
 
 module.exports = {
-    decode
-}
+  decode,
+};
